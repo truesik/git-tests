07c12197a178397e6bdf597a4dc39997ad3913b4
truesik <truesik@gmail.com>
2020-03-28 14:06:19 +0300

    very important changes

--- src/main/java/org/truesik/Application.java
+++ src/main/java/org/truesik/Application.java
@@ -3,6 +3,6 @@ package org.truesik;
 public class Application {
     public static void main(String[] args) {
         Test test = new Test();
-        test.someMethod();
+        test.printHello();
     }
 }
---
+++ src/main/java/org/truesik/Test.java
@@ -0,0 +1,14 @@
+package org.truesik;
+
+public class Test {
+    public Test() {
+    }
+
+    public void printHello() {
+        System.out.println(getHello());
+    }
+
+    private String getHello() {
+        return "hello";
+    }
+}
