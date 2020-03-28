cf3885e5726825f51d21dd3d8a455999138708af
truesik <truesik@gmail.com>
2020-03-28 14:01:05 +0300

    git init

---
+++ src/org/truesik/Application.java
@@ -0,0 +1,8 @@
+package org.truesik;
+
+public class Application {
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.someMethod();
+    }
+}
---
+++ src/org/truesik/Test.java
@@ -0,0 +1,10 @@
+package org.truesik;
+
+public class Test {
+    public Test() {
+    }
+
+    public void someMethod() {
+        System.out.println("hello");
+    }
+}
