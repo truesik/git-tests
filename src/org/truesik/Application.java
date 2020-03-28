5a6d882c4e640c49f4ae1a872337659ead86407b
truesik <truesik@gmail.com>
2020-03-28 14:08:23 +0300

    changes in master

--- src/org/truesik/Application.java
+++ src/org/truesik/Application.java
@@ -4,5 +4,6 @@ public class Application {
     public static void main(String[] args) {
         Test test = new Test();
         test.someMethod();
+        test.printBuy();
     }
 }
--- src/org/truesik/Test.java
+++ src/org/truesik/Test.java
@@ -4,6 +4,10 @@ public class Test {
     public Test() {
     }
 
+    public void printBuy() {
+        System.out.println("Buy");
+    }
+
     public void someMethod() {
         System.out.println("hello");
     }
